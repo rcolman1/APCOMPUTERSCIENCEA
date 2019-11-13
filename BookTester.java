323bf9b78f9dcdda4830f69496bbc902578fc703
rcolman1 <52420838+rcolman1@users.noreply.github.com>
2019-11-13 09:19:25 -0800

    Add files via upload

--- BookTester.java
+++ BookTester.java
@@ -8,5 +8,19 @@ public class BookTester
      Books b2 = new Books("The Highly Sensitive Person", "Elaine Aron", 200);
      System.out.println(b1);
      System.out.println(b2);
+     b1.setTitle("Invisible Man");
+     System.out.println(b1);
+     b1.setAuthor("Ralph Ellison");
+     System.out.println(b1);
+     b1.setpageNumbers(500);
+     System.out.println(b1);
+     b2.setTitle("Their Eyes were Watching God");
+     System.out.println(b2);
+     b2.setAuthor("Zora Neale Hurston");
+     System.out.println(b2);
+     b2.setpageNumbers(499);
+     System.out.println(b2);
+     
+     
     }
 }
